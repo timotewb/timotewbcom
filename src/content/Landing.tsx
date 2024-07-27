5c1a2d74287c32e623b631764fd8acf47bffbf9e
timotewb <45491056+timotewb@users.noreply.github.com>
2024-07-27 22:08:00 +1200

    Eatwords (#1)
    * animation dev
    
    * eatword dev
    
    * fadein fadeout eatwords
    
    * greeting
    
    ---------
    
    Co-authored-by: twipiiti-benseman <twipiiti-benseman@kpmg.co.nz>

--- src/content/Landing.tsx
+++ src/content/Landing.tsx
@@ -1,66 +1,79 @@
-import { useState, useEffect } from 'react';
-import useGlobalKeyboardListener from './useGlobalKeyboardListener';
-import useGlobalMouseClickListener from './useGlobalMouseClickListener';
-import './Landing.css'
+import { useState, useEffect } from "react";
+import useGlobalKeyboardListener from "./useGlobalKeyboardListener";
+import "./Landing.css";
 
 function Landing() {
-    const [displayText, setDisplayText] = useState<string>('chur');
-    const [keyString, setKeyString] = useState<string>('');
-    const [mouseString, setMouseString] = useState<string>('');
-    const [shouldFadeOut, setShouldFadeOut] = useState<boolean>(false);
-    const [canChange, setCanChange] = useState<boolean>(true);
+  const [displayText, setDisplayText] = useState<string>("chur");
+  const [keyString, setKeyString] = useState<string>("");
+  const [shouldFadeOut, setShouldFadeOut] = useState<boolean>(false);
+  const [canChange, setCanChange] = useState<boolean>(true);
+  const [showAnimation, setShowAnimation] = useState<boolean>(false);
+  const [canEat, setCanEat] = useState<boolean>(false);
 
-    useGlobalMouseClickListener((timeDifference) => {
-      if (mouseString.length > 100){
-        setMouseString("");
+  const addSpan = (note: string) => {
+    type SpanStyleType = {
+      animationDelay?: string;
+      paddingLeft?: string;
+      // Add any other CSS properties you plan to use here
+    };
+    return note.split("").map((letter, index) => {
+      index = index / 2;
+      let spanStyle: SpanStyleType = { animationDelay: `${index}s` };
+      // Check if the letter is a whitespace character and adjust styling if needed
+      if (/[\s]/.test(letter)) {
+        spanStyle = { ...spanStyle, paddingLeft: ".5em" };
+        letter = "";
       }
-      if (timeDifference < 100) {
-        console.log('. Dot', timeDifference); // Dot
-        setMouseString(mouseString => mouseString + ".");
-      } else {
-        console.log('- Dash', timeDifference); // Dash
-        setMouseString(mouseString => mouseString + "-");
-      }
-      console.log(mouseString);
+      return (
+        <span style={spanStyle} key={index}>
+          {letter}
+        </span>
+      );
     });
+  };
 
-    useGlobalKeyboardListener((event, currentKey) => {
-      if (keyString.length > 100){
-        setKeyString("");
-      }
-      setKeyString(keyString => keyString + currentKey);
-      console.log(keyString);
-          }, keyString);
+  useGlobalKeyboardListener((event, currentKey) => {
+    if (keyString.length > 100) {
+      setKeyString("");
+    }
+    setKeyString((keyString) => keyString + currentKey);
+  }, keyString);
 
-    useEffect(() => {
-        if (keyString === 'chur' && canChange) {
-          setKeyString("");
-          setCanChange(false);
-          setShouldFadeOut(true);
-          setTimeout(() => {
-            setDisplayText('chur bro'); // Change to the next text after delay
-            setShouldFadeOut(false);
-          }, 2000); // Delay to allow fade-out animation
-        }
-        if (mouseString === "-.-." && canChange){
-          setMouseString("");
-          setCanChange(false);
-          setShouldFadeOut(true);
-          setTimeout(() => {
-            setDisplayText('chur bro'); // Change to the next text after delay
-            setShouldFadeOut(false);
-          }, 2000); // Delay to allow fade-out animation
+  useEffect(() => {
+    if (keyString === "chur" && canChange) {
+      setKeyString("");
+      setCanChange(false);
+      setShouldFadeOut(true);
+      setShowAnimation(true);
 
-        }
-      }, [keyString, mouseString, canChange]);
+      // wait for initial text to fadeOut, then start fadeIn of new text
+      setTimeout(() => {
+        setDisplayText("chur bro");
+        setShouldFadeOut(false);
+      }, 2000);
+
+      // Once new text is displayed, start eating
+      setTimeout(() => {
+        setCanEat(true);
+      }, 7000);
+    }
+  }, [keyString, canChange, showAnimation, canEat]);
 
   return (
-    <p>
-<p className={`fade ${shouldFadeOut ? 'fade-out' : ''}`}>{displayText}</p>
-    </p>
+    <>
+      <div className="Content">
+        <div className="landingH1">
+          <span
+            className={` ${
+              shouldFadeOut ? "landingH1-fadeOut" : "landingH1-fadeIn"
+            } ${canEat ? "eatwords" : ""} `}
+          >
+            {addSpan(displayText)}
+          </span>
+        </div>
+      </div>
+    </>
   );
 }
 
 export default Landing;
-
-
