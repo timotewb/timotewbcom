1edfeb6ce2e37cd734398a1a960d2f7c4fc4498f
timotewb <timote.wb@gmail.com>
2024-08-01 21:04:39 +1200

    v2 dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-
-import Landing from './content/Landing';
-import './App.css';
+import React, { useState, useRef } from "react";
+import "./App.css";
 
 function App() {
+  const prefix = "<span className='App-input-prfx'>?></span>";
+  const suffix = "<span className='App-input-prfx'>|</span>";
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [inputText, setInputText] = useState(`${prefix}`);
+
+  const handleInputChange = (event: React.FormEvent<HTMLDivElement>) => {
+    let newValue = "";
+    if (event.currentTarget.textContent) {
+      console.log(event.currentTarget.textContent);
+      const currentValue = event.currentTarget.textContent;
+      newValue = `${prefix}${currentValue}${suffix}`;
+    }
+    setInputText(newValue);
+  };
+
   return (
     <div className="App">
-        <Landing />
+      <div className="App-header">
+        <div className="App-greeting">welcome</div>
+      </div>
+      <div className="App-input-container">
+        <div
+          className="App-input-text"
+          contentEditable
+          tabIndex={0}
+          onChange={handleInputChange}
+          // onKeyDown={handleInputChange}
+          dangerouslySetInnerHTML={{ __html: inputText }}
+        ></div>
+      </div>
     </div>
   );
 }
